f86e7fa18c042512595618ac95bfff8b6c0d26e3
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2023-10-13 10:32:22 +0100

    infer after train

--- cesped/trainEntry.py
+++ cesped/trainEntry.py
@@ -1,6 +1,7 @@
 import os
 import os.path as osp
 import shutil
+import subprocess
 import sys
 
 import psutil
@@ -13,6 +14,14 @@ from cesped.network.plModule import PlModel
 # from cesped.datamanager.dataManager import ParticlesDataModule
 from cesped.datamanager.plDataset import ParticlesDataModule
 
+
+
+_skip_other_half_inference = "skip_other_half_inference"
+class _TrainCLI(MyLightningCLI):
+    def add_arguments_to_parser(self, parser):
+        super().add_arguments_to_parser(parser)
+        parser.add_argument(f"--{_skip_other_half_inference}", action="store_true", help="If true, "
+                                             "after training finishes, inference is NOT called on the other half dataset")
 def _copyCodeForReproducibility(logdir):
     """
     Copy the code to the logdir so that reproducibility is ensured
@@ -49,7 +58,6 @@ def _copyCodeForReproducibility(logdir):
     fname = osp.join(logdir, "command.txt")
     with open(fname, "w") as f:
         f.write(" ".join(sys.argv))
-
 if __name__ == "__main__":
 
 
@@ -61,18 +69,35 @@ if __name__ == "__main__":
                      ]
 
     with CheckpointLoader(config_fnames) as cpk:
-
-        cli = MyLightningCLI(model_class=PlModel,
-                             datamodule_class=ParticlesDataModule,
-                             parser_kwargs={"default_env": True, "parser_mode":"omegaconf",
+        cli = _TrainCLI(model_class=PlModel,
+                        datamodule_class=ParticlesDataModule,
+                        parser_kwargs={"default_env": True, "parser_mode":"omegaconf",
                                             "default_config_files": cpk.config_fnames},
-                             run=False)
+                        run=False)
         trainer = cli.trainer
         logdir = cli.trainer.log_dir
         if trainer.is_global_zero:
             _copyCodeForReproducibility(logdir)
 
         trainer.fit(cli.model, cli.datamodule, ckpt_path=cpk.ckpt_path)
+
+        cli.trainer.strategy.barrier()
+        if not cli.config.get(_skip_other_half_inference, False) and trainer.is_global_zero:
+            otherHalf = (cli.config["data"]["halfset"] + 1) % 2
+            print(f"Running inference on the halfset {otherHalf} ")
+
+            cmd = [
+                "python", "-m",  "cesped.inferEntry",
+                "--data.targetName" , cli.config["data"]["targetName"],
+                "--data.halfset", str(otherHalf),
+                "--data.num_data_workers", str(cli.config["data"]["num_data_workers"]),
+                "--data.batch_size", str(cli.config["data"]["batch_size"]),
+                "--outFname", osp.join(logdir, f"predictions_{otherHalf}.star"),
+                "--ckpt_path", getattr(cli.trainer.checkpoint_callback, "best_model_path", "best")]
+            print(" ".join(cmd))
+            subprocess.run(cmd, check=True, cwd=osp.dirname(osp.dirname(__file__)))
+
+
 
 """
 -c ../configs/defaultTrainerConfig.yaml -c ../configs/defaultDataConfig.yaml -c ./configs/defaultModelConfig.yaml \ 
