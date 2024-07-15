e02442e3c0f8e5f0e4f845705ea1fdb0b56742a6
Ruben Sanchez Garcia <rubensanchezgarc@gmail.com>
2024-07-15 13:11:21 +0100

    bug fft freq

--- cesped/__init__.py
+++ cesped/__init__.py
@@ -4,6 +4,6 @@ Utilities to train and evaluate deep learning models for Cryo-EM Supervised Part
 """
 
 
-__version__ = "24.06.0"
+__version__ = "24.07.0"
 
 from cesped.datamanager.relionStarDataset import ParticlesRelionStarDataset
